c6e3c88cc0182997aa7e5bb84bda884128c8c4e0
Haileabe <haileabe@addissoftware.com>
2023-04-22 11:29:09 +0300

    added all routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,6 +11,39 @@ export const getUser = async (req, res) => {
   }
 };
 
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).send(users);
+  } catch (error) {
+    return res.status(400).send("Users not found");
+  }
+};
+
+// export const addUser = async (req, res, next) => {
+//   try {
+//     const newUser = new User(req.body);
+//     await newUser.save();
+//     res.status(200).send(newUser);
+//   } catch (error) {
+//     next(error);
+//   }
+// }
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (req.userId !== user._id.toString()) {
+      return next(createError(403, "You can update only your account!"));
+    }
+    await User.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).send("Successfully updated!");
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const deleteUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { getUser, deleteUser } from "../controllers/user.controller.js";
+import {
+  getUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.get("/:id", getUser);
+router.get("/", verifyToken, getUser);
 router.delete("/:id", verifyToken, deleteUser);
-// router.get("/",getUsers)
+router.put("/:id", verifyToken, updateUser);
 
 export default router;
